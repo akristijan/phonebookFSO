542912926efa89200e9956fee140d1399fe0dc72
unknown <Administrator@BOSANSKAKRUPA.LOCAL>
2023-11-14 14:38:41 +0100

    save action to mongo db  works

--- index.js
+++ index.js
@@ -20,29 +20,6 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 
 
-/* let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
- */
 //Routes
 app.get('/', (req, res) => {
     return res.send("<h1>Welcome to Phonebook</h1>")
@@ -52,8 +29,8 @@ app.get('/api/persons', async (req, res) => {
     try {
         const contacts = await Person.find().lean()
         console.log(contacts)
-        mongoose.connection.close()
         res.json(contacts)
+        
         
     } catch (error) {
         console.log(error)
@@ -100,7 +77,7 @@ app.post('/api/persons', (req,res) => {
 
     newPerson.save().then(result => {
         console.log("Contact saved!")
-        mongoose.connection.close()
+        
     })
 
    /*  if(phonebook.find(p=> p.name === newPerson.name)) {
@@ -112,11 +89,11 @@ app.post('/api/persons', (req,res) => {
     res.json(newPerson)
 })
 
-app.delete('/api/persons/:id', (req,res) => {
-    const id = Number(req.params.id)
-    phonebook = phonebook.filter(p => p.id !== id)
+/* app.delete('/api/persons/:id',async (req,res) => {
+    
+    await Person.deleteOne({_id: ObjectId(req.params.id)})
     res.status(204).end()
-})
+}) */
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
